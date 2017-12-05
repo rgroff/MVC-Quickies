db5f6b19b4909b216df4cf9d5d9ce00096f6ef28
Ryon Groff <ryongroff@live.com>
2017-12-04 20:56:35 -0600

    MVC Web Api Example
    Using DTO and AutoMapper

---
+++ CustomersController.cs
@@ -0,0 +1,95 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.DTOs;
+using Vidly.Models;
+
+namespace Vidly.Controllers.API
+{
+    public class CustomersController : ApiController
+    {
+
+        private MyDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new MyDbContext();
+        }
+
+        /// Get /api/customers
+    
+        public IEnumerable<CustomerDTO> GetCustomers()
+        {
+            return _context.Customers.ToList().Select(Mapper.Map<Customer,CustomerDTO>);
+        }
+
+        ///Get /api/customers/1
+        
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(Mapper.Map<Customer,CustomerDTO>(customer));
+        }
+
+        //Post /api/customers
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(CustomerDTO customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var customer = Mapper.Map<CustomerDTO, Customer>(customerDto);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            customerDto.Id = customer.Id;
+            return Created(new Uri(Request.RequestUri + "/" + customer.Id),
+                customerDto);
+        }
+
+        //Put  /api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id,CustomerDTO customerDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var customerinDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerinDB == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            Mapper.Map(customerDto, customerinDB);
+
+            _context.SaveChanges();
+        }
+
+
+        //DELETE 
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerinDB = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerinDB == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+            _context.Customers.Remove(customerinDB);
+            _context.SaveChanges();
+        }
+    }
+}
